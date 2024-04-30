a980df07e15163673f7609a3c4c2829f72d9a38b
Reza Karegar <karegar@gmail.com>
2024-04-30 19:00:35 -0400

    Create examples for the connector.

---
+++ examples/scala-sbt/README.md
@@ -0,0 +1,105 @@
+# Spark Bigtable Example Using Scala and sbt
+
+This example uses Apache Spark with Scala and sbt for package management to
+write data to a Bigtable table and read it back.
+
+## Compiling the project
+
+To compile the code, you can run
+the following command (after installing sbt) from inside the current
+directory:
+
+```
+sbt clean assembly
+```
+
+The target JAR will be located under
+`target/scala-2.12/spark-bigtable-example-assembly-0.1.jar`.
+
+## Running the example
+
+To run the JAR, you will need a Bigtable project and instance ID, as well as a
+Bigtable table name, which will be the three required arguments. By default, a
+new table is created by the application, but you can provide an optional fourth
+argument `false` for `createNewTable` to avoid doing so. (Assuming that you have
+already created a table with the column family `example_family`.)
+
+To run the job using dataproc, you can run the following command:
+
+```shell
+gcloud dataproc jobs submit spark \
+--cluster=$BIGTABLE_SPARK_DATAPROC_CLUSTER \
+--region=$BIGTABLE_SPARK_DATAPROC_REGION \
+--class=spark.bigtable.example.WordCount \
+--jars=target/scala-2.12/spark-bigtable-example-assembly-0.1.jar  \
+--  \
+$BIGTABLE_SPARK_PROJECT_ID \
+$BIGTABLE_SPARK_INSTANCE_ID \
+$BIGTABLE_SPARK_TABLE_NAME
+```
+
+## Using local Spark
+
+You can run the following command to run the example locally,
+assuming you have Spark installed on your machine:
+
+```shell
+spark-submit \
+target/scala-2.12/spark-bigtable-example-assembly-0.1.jar \
+$BIGTABLE_SPARK_PROJECT_ID \
+$BIGTABLE_SPARK_INSTANCE_ID \
+$BIGTABLE_SPARK_TABLE_NAME
+```
+
+## Expected output
+
+The following text should be shown in the output of the Spark job.
+
+```
+Reading the DataFrame from Bigtable:
++-----+-----+----------------+
+|count| word|frequency_double|
++-----+-----+----------------+
+|    0|word0|             0.0|
+|    1|word1|           0.001|
+|    2|word2|           0.002|
+|    3|word3|           0.003|
+|    4|word4|           0.004|
+|    5|word5|           0.005|
+|    6|word6|           0.006|
+|    7|word7|           0.007|
+|    8|word8|           0.008|
+|    9|word9|           0.009|
++-----+-----+----------------+
+```
+
+
+To verify that the data has been written to Bigtable, you can run the following
+command (requires [cbt CLI](https://cloud.google.com/bigtable/docs/cbt-overview)):
+
+```
+cbt -project=$BIGTABLE_SPARK_PROJECT_ID -instance=$BIGTABLE_SPARK_INSTANCE_ID \
+read $BIGTABLE_SPARK_TABLE_NAME
+```
+
+With this expected output:
+```
+----------------------------------------
+word0
+  example_family:countCol                  @ 2024/04/30-14:54:16.401000
+    "\x00\x00\x00\x00\x00\x00\x00\x00"
+  example_family:frequencyCol              @ 2024/04/30-14:54:16.401000
+    "\x00\x00\x00\x00\x00\x00\x00\x00"
+
+----------------------------------------
+word1
+  example_family:countCol                  @ 2024/04/30-14:54:16.401000
+    "\x00\x00\x00\x00\x00\x00\x00\x01"
+  example_family:frequencyCol              @ 2024/04/30-14:54:16.401000
+    "?PbM\xd2\xf1\xa9\xfc"
+
+----------------------------------------
+.
+.
+.
+```
---
+++ examples/scala-sbt/src/main/scala/spark/bigtable/example/WordCount.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package spark.bigtable.example
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.functions.col
+import java.nio.ByteBuffer
+
+object WordCount extends App {
+  val (projectId, instanceId, tableName, createNewTable) = parse(args)
+
+  val spark = SparkSession.builder().getOrCreate()
+
+  val doubleToBinaryUdf = udf((value: Double) => doubleToBinary(value))
+  val binaryToDoubleUdf =
+    udf((binaryData: Array[Byte]) => binaryToDouble(binaryData))
+
+  val catalog =
+    s"""{
+       |"table":{"namespace":"default", "name":"$tableName", "tableCoder":"PrimitiveType"},
+       |"rowkey":"wordCol",
+       |"columns":{
+       |  "word":{"cf":"rowkey", "col":"wordCol", "type":"string"},
+       |  "count":{"cf":"example_family", "col":"countCol", "type":"long"},
+       |  "frequency_binary":{"cf":"example_family", "col":"frequencyCol", "type":"binary"}
+       |}
+       |}""".stripMargin
+
+  import spark.implicits._
+  val data = (0 to 9).map(i => ("word%d".format(i), i, i / 1000.0))
+  val rdd = spark.sparkContext.parallelize(data)
+  val dfWithDouble = rdd.toDF("word", "count", "frequency_double")
+
+  println("Created the DataFrame:");
+  dfWithDouble.show()
+
+  val dfToWrite = dfWithDouble
+    .withColumn(
+      "frequency_binary",
+      doubleToBinaryUdf(dfWithDouble.col("frequency_double"))
+    )
+    .drop("frequency_double")
+
+  dfToWrite.write
+    .format("bigtable")
+    .option("catalog", catalog)
+    .option("spark.bigtable.project.id", projectId)
+    .option("spark.bigtable.instance.id", instanceId)
+    .option("spark.bigtable.create.new.table", createNewTable)
+    .save
+  println("DataFrame was written to Bigtable.")
+
+  val readDf = spark.read
+    .format("bigtable")
+    .option("catalog", catalog)
+    .option("spark.bigtable.project.id", projectId)
+    .option("spark.bigtable.instance.id", instanceId)
+    .load
+
+  val readDfWithDouble = readDf
+    .withColumn(
+      "frequency_double",
+      binaryToDoubleUdf(readDf.col("frequency_binary"))
+    )
+    .drop("frequency_binary")
+
+  println("Reading the DataFrame from Bigtable:");
+  readDfWithDouble.show()
+
+  def parse(args: Array[String]): (String, String, String, String) = {
+    import scala.util.Try
+    val projectId = Try(args(0)).getOrElse {
+      throw new IllegalArgumentException(
+        "Missing command-line argument: SPARK_BIGTABLE_PROJECT_ID"
+      )
+    }
+    val instanceId = Try(args(1)).getOrElse {
+      throw new IllegalArgumentException(
+        "Missing command-line argument: SPARK_BIGTABLE_INSTANCE_ID"
+      )
+    }
+    val tableName = Try(args(2)).getOrElse {
+      throw new IllegalArgumentException(
+        "Missing command-line argument: SPARK_BIGTABLE_TABLE_NAME"
+      )
+    }
+    val createNewTable = Try(args(3)).getOrElse {
+      "true"
+    }
+    (projectId, instanceId, tableName, createNewTable)
+  }
+
+  def doubleToBinary(value: Double): Array[Byte] = {
+    ByteBuffer.allocate(java.lang.Double.BYTES).putDouble(value).array()
+  }
+
+  def binaryToDouble(binaryData: Array[Byte]): Double = {
+    if (binaryData == null || binaryData.length == 0) {
+      Double.NaN
+    } else {
+      ByteBuffer.wrap(binaryData).getDouble()
+    }
+  }
+}
