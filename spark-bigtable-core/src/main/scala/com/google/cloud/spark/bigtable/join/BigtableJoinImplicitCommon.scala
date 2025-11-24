f83d5be8ec36020640ed01c8b61219adac05374f
Mattie Fu <mattiefu@google.com>
2025-11-24 16:57:13 -0500

    update join

--- spark-bigtable-core/src/main/scala/com/google/cloud/spark/bigtable/join/BigtableJoinImplicitCommon.scala
+++ spark-bigtable-core/src/main/scala/com/google/cloud/spark/bigtable/join/BigtableJoinImplicitCommon.scala
@@ -8,7 +8,7 @@ import org.apache.spark.sql.{Column, DataFrame, SparkSession, Row => SparkRow}
 import com.google.api.gax.rpc.ServerStream
 import com.google.cloud.bigtable.data.v2.models.{Query, TableId, Row => BigtableRow}
 import com.google.cloud.spark.bigtable.datasources.config.BigtableClientConfig
-import com.google.cloud.spark.bigtable.datasources.config.application.SparkScanConfig.ROW_FILTERS_CONFIG_KEY
+import com.google.cloud.spark.bigtable.datasources.config.application.SparkScanConfig.{PUSH_DOWN_COLUMN_FILTERS_CONFIG_KEY, ROW_FILTERS_CONFIG_KEY}
 import com.google.cloud.spark.bigtable.util.RowFilterUtils
 import com.google.protobuf.ByteString
 import org.apache.spark.SparkContext
@@ -99,16 +99,19 @@ object BigtableJoinImplicitCommon extends Serializable with Logging {
       getBigtableConfig(parameters, bigtableSchema, catalog, spark.sparkContext)
     val btRowKeyField = catalog.sMap.fields.find(_.isRowKey).get
     val srcRowKeyField = btRowKeyField.copy(sparkColName = srcRowKeyCol)
+    // column filters from catalog
+    val pushDownColumnFilters = parameters.getOrElse(PUSH_DOWN_COLUMN_FILTERS_CONFIG_KEY, "true").toBoolean
+    val columnFilters = SparkSqlFilterAdapter.createColumnFilter(catalog, pushDownColumnFilters)
+    // other complex filters
     val rowFilterString = parameters.get(ROW_FILTERS_CONFIG_KEY)
     val rowFilters = rowFilterString.map(RowFilterUtils.decode).getOrElse(FILTERS.pass())
-    val columnFilters = SparkSqlFilterAdapter.createColumnFilter(catalog)
     // Fetch Bigtable rows based on join keys from the source DataFrame
     val bigtableRdd = srcDf
       .select(srcRowKeyCol)
       .rdd
       .mapPartitionsWithIndex { (partitionIndex, rows) =>
         val rowKeys = rows.map(r => srcRowKeyField.scalaValueToBigtable(r.getAs[Any](srcRowKeyCol)))
-        val btRows = fetchBigtableRows(rowKeys, FILTERS.chain().filter(rowFilters).filter(columnFilters), bigtableConfig, catalog.name, partitionIndex)
+        val btRows = fetchBigtableRows(rowKeys, FILTERS.chain().filter(columnFilters).filter(rowFilters), bigtableConfig, catalog.name, partitionIndex)
         btRows
           .filter(_ != null)
           .flatMap(row => ReadRowConversions.buildRow(orderedFields, row, catalog))
